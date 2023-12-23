f868b2e0b281d0e8ee8099c4466f2b100ad7c277
Splafty <Splaft@wp.pl>
2023-12-23 22:21:20 +0100

    Table changes

--- script.js
+++ script.js
@@ -28,3 +28,77 @@ sr.reveal(".navigation-menu", {delay: 300, duration: 3000, reset: false, origin:
 //     event.currentTarget.classList.add("active-link");
 //     document.getElementById(tabname).classList.add("active-tab");
 // }
+
+// Function to fetch JSON data
+function fetchJSON() {
+    // Fetch JSON data from the API
+    fetch("https://my.api.mockaroo.com/Web-Dev-Ranking.json?key=d08f0cf0")
+        .then(response => response.json()) // Convert the response into JSON format
+        .then(data => {
+            const items = data.slice(0, 10); // Take only the first 10 items for demonstration
+
+            // Get the table body element by ID
+            const tableBody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
+
+            // Iterate over items
+            items.forEach(item => {
+                // Create a table row element for each item in the JSON
+                const tr = document.createElement("tr");
+
+                // Iterate over properties in the current item
+                for (const key in item) {
+                    if (item.hasOwnProperty(key)) {
+                        // Create a table cell for each property and import the data
+                        const td = document.createElement("td");
+                        
+                        // Add content
+                        td.textContent = item[key];
+                        
+                        tr.appendChild(td); // Add the created cell to the current row
+                    }
+                }
+
+                // Add the created row to the table body
+                tableBody.appendChild(tr);
+            });
+        })
+        .catch(error => console.error("Error fetching JSON data:", error)); // Log an error if fetching fails
+}
+
+fetchJSON();
+
+// // tworzenie tabeli
+// async function getJson() {
+//     const response = await fetch("https://my.api.mockaroo.com/Web-Dev-Ranking.json?key=d08f0cf0");
+//     const data = await response.json();
+//     createTables(data)
+// }
+
+// let tables = ['first_name', 'last_name', 'num_5_star_reviews', 'total_websites_created', 'portfolio_link']
+// // total websites spacja jest i sie pierdoli tabelka
+
+// function createTables(dane){
+//     var daneArray = dane
+//     const table = document.querySelector('#table') // szukanie tabelki w html
+    
+//     let newTr = document.createElement('tr') 
+//     table.append(newTr)
+//     for(q in tables){
+//         let newTh = document.createElement('th')
+//         newTh.innerHTML = tables[q]
+//         newTr.append(newTh)
+//     } // tworzenie nagłówków tabeli i przypisanie danych z api
+
+//     for(k in daneArray){
+//         let newTd = document.createElement('tr')
+//         table.append(newTd)
+//         for(v in tables){
+//             let wiersz = k
+//             let td = document.createElement('td')
+//             td.innerHTML = daneArray[wiersz][tables[v]]
+//             newTd.append(td)
+//         }
+//     }
+// } // tworzenie wierszy i kolumn tabeli i przypisanie danych z api
+
+// getJson()
