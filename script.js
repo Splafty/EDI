0a59efd84059e52772a6a1dfa90aaa8fe6935523
Splafty <Splaft@wp.pl>
2023-12-28 15:03:43 +0100

    Charts sketch and minor code changes

--- script.js
+++ script.js
@@ -9,63 +9,54 @@ const sr = ScrollReveal ({
 sr.reveal(".home-text", {delay: 100, reset: false, origin: "top"});
 sr.reveal(".navigation-menu", {delay: 300, duration: 3000, reset: false, origin: "right"});
 
-// // Recycle??
 
-// var tablinks = document.getElementsByClassName("tab-links");
-// var tabcontents = document.getElementsByClassName("tab-contents");
-
-// function opentab(event, tabname)
-// {
-//     for(tablink of tablinks)
-//     {
-//         tablink.classList.remove("active-link");
-//     }
-    
-//     for(tabcontent of tabcontents)
-//     {
-//         tabcontent.classList.remove("active-tab");
-//     }
-//     event.currentTarget.classList.add("active-link");
-//     document.getElementById(tabname).classList.add("active-tab");
-// }
 
 // Function to fetch JSON data
 function fetchJSON() {
-    // Fetch JSON data from the API
-    fetch("https://my.api.mockaroo.com/Web-Dev-Ranking.json?key=d08f0cf0")
+    fetch("https://my.api.mockaroo.com/Web-Dev-Ranking.json?key=d08f0cf0") // Fetch JSON data from the API
         .then(response => response.json()) // Convert the response into JSON format
         .then(data => {
             const items = data.slice(0, 10); // Take only the first 10 items for demonstration
 
+            // Sort items by most 5_star_reviews
+            items.sort((a, b) => b["num_5_star_reviews"] - a["num_5_star_reviews"]);
+
             // Get the table body element by ID
             const tableBody = document.getElementById("dataTable").getElementsByTagName("tbody")[0];
 
+            // Position variable
+            var position = 1
             // Iterate over items
             items.forEach(item => {
-                // Create a table row element for each item in the JSON
-                const tr = document.createElement("tr");
+                
+                const tr = document.createElement("tr"); // Create a table row element for each item in the JSON
+
+                const rank = document.createElement("td");    // Create a table cell for position variable
+                rank.textContent = position;                  // Add position variable as content
+                tr.appendChild(rank);                         // Add the created cell to the current row
 
                 // Iterate over properties in the current item
-                for (const key in item) {
-                    if (item.hasOwnProperty(key)) {
-                        // Create a table cell for each property and import the data
-                        const td = document.createElement("td");
-                        
-                        // Add content
-                        td.textContent = item[key];
-                        
-                        tr.appendChild(td); // Add the created cell to the current row
+                for (const key in item) 
+                {
+                    if (item.hasOwnProperty(key)) 
+                    {   
+                        const td = document.createElement("td");    // Create a table cell for each property and import the data
+                        td.textContent = item[key];                 // Add content
+                        tr.appendChild(td);                         // Add the created cell to the current row
                     }
                 }
-
-                // Add the created row to the table body
-                tableBody.appendChild(tr);
+                position++; // 
+                tableBody.appendChild(tr); // Add the created row to the table body
             });
         })
         .catch(error => console.error("Error fetching JSON data:", error)); // Log an error if fetching fails
 }
 
 fetchJSON();
+
+
+
+
 
 // // tworzenie tabeli
 // async function getJson() {
@@ -102,3 +93,28 @@ fetchJSON();
 // } // tworzenie wierszy i kolumn tabeli i przypisanie danych z api
 
 // getJson()
+
+
+
+
+
+
+// // Recycle??
+
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(event, tabname)
+// {
+//     for(tablink of tablinks)
+//     {
+//         tablink.classList.remove("active-link");
+//     }
+    
+//     for(tabcontent of tabcontents)
+//     {
+//         tabcontent.classList.remove("active-tab");
+//     }
+//     event.currentTarget.classList.add("active-link");
+//     document.getElementById(tabname).classList.add("active-tab");
+// }
